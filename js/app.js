7c7e263c785398045d7acf283f4de74abda9c7af
Dan Riding <doonyby@gmail.com>
2016-04-06 17:47:00 -0600

    added counter

--- js/app.js
+++ js/app.js
@@ -1,38 +1,86 @@
 
 $(document).ready(function() {
-	
-	var mysteryNumber = parseInt((Math.random() * 100) + 1);
-	console.log(mysteryNumber);
-
-	$('#userinput').submit(function(e) {
-		e.preventDefault();
-		$('#guessList').append('<li>' + ($('#userGuess').val()) + '</li>');
-		var shoot = parseInt($('#userGuess').val());
-			if (shoot === mysteryNumber) {
-				$('#feedback').text('You won!  Click new game to play again!');
-			}
-			else {
-				$('#feedback').text('Cold!');
-			}
-	
 		
-		$('#userGuess').val('');
-	})
+		var mysteryNumber = parseInt((Math.random() * 100) + 1);
+		console.log(mysteryNumber);
+		var counter = 0;
 
+		$('#userinput').submit(function(e) {
+			e.preventDefault();
+			counter++;
+			$('#count').text(counter)
+			$('#guessList').append('<li>' + ($('#userGuess').val()) + '</li>');
+			var guess = parseInt($('#userGuess').val(), 10);
+				if (guess === mysteryNumber) {
+					$('#feedback').text('You won!  Click new game to play again!');
+					$('#guessButton').css({"visibility":"hidden"});
+				}
+				else if (guess < (mysteryNumber - 30) && guess > (mysteryNumber + 30)) {
+					$('#feedback').text('Cold!');
+				}
+				else if (guess < (mysteryNumber - 20) && guess > (mysteryNumber + 20)) {
+					$('#feedback').text('Warm!');
+				}
+				else if (guess < (mysteryNumber - 10) && guess > (mysteryNumber + 10)) {
+					$('#feedback').text('Hot!');
+				}
+				else if (guess < (mysteryNumber - 5) && guess > (mysteryNumber + 5)) {
+					$('#feedback').text('Very hot!');
+				}
+				else {
+					$('#feedback').text('Extremely hot!');
+				}
+		
+			$('#userGuess').val('');
+		})
+
+function newGame() {
+		$('.new').click(function() {
+		var mysteryNumber = parseInt((Math.random() * 100) + 1);
+		console.log(mysteryNumber);
+		var counter = 0;
+
+		$('#userinput').submit(function(e) {
+			e.preventDefault();
+			counter++;
+			$('#count').text(counter)
+			$('#guessList').append('<li>' + ($('#userGuess').val()) + '</li>');
+			var guess = parseInt($('#userGuess').val(), 10);
+				if (guess === mysteryNumber) {
+					$('#feedback').text('You won!  Click new game to play again!');
+					$('#guessButton').css({"visibility":"hidden"});
+				}
+				else if (guess < (mysteryNumber - 30) && guess > (mysteryNumber + 30)) {
+					$('#feedback').text('Cold!');
+				}
+				else if (guess < (mysteryNumber - 20) && guess > (mysteryNumber + 20)) {
+					$('#feedback').text('Warm!');
+				}
+				else if (guess < (mysteryNumber - 10) && guess > (mysteryNumber + 10)) {
+					$('#feedback').text('Hot!');
+				}
+				else if (guess < (mysteryNumber - 5) && guess > (mysteryNumber + 5)) {
+					$('#feedback').text('Very hot!');
+				}
+				else {
+					$('#feedback').text('Extremely hot!');
+				}
+		
+			$('#userGuess').val('');
+		})
+		})
+	}
 
 });
 
-/*var clicks = 0;
-function linkClick() {
-    document.getElementById('clicked').value = ++clicks;
-}*/
 
-/*function newGame() {
-		$('.new').click(function() {
-			$('.game').empty();
-		})
-	}*/
 
+/*$('.new').click(function() {
+			$('#feedback, #userGuess, #guessButton, #count').reset();
+		})*/
+
+
+	
 
 
 
