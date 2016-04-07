c2638cae799ce005fd2c4e0c8fe9216aa24c6058
Dan Riding <doonyby@gmail.com>
2016-04-06 18:15:33 -0600

    included modal

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function() {
+$(document).ready(function hotncold() {
 		
 		var mysteryNumber = parseInt((Math.random() * 100) + 1);
 		console.log(mysteryNumber);
@@ -33,56 +33,6 @@ $(document).ready(function() {
 		
 			$('#userGuess').val('');
 		})
-
-function newGame() {
-		$('.new').click(function() {
-		var mysteryNumber = parseInt((Math.random() * 100) + 1);
-		console.log(mysteryNumber);
-		var counter = 0;
-
-		$('#userinput').submit(function(e) {
-			e.preventDefault();
-			counter++;
-			$('#count').text(counter)
-			$('#guessList').append('<li>' + ($('#userGuess').val()) + '</li>');
-			var guess = parseInt($('#userGuess').val(), 10);
-				if (guess === mysteryNumber) {
-					$('#feedback').text('You won!  Click new game to play again!');
-					$('#guessButton').css({"visibility":"hidden"});
-				}
-				else if (guess < (mysteryNumber - 30) && guess > (mysteryNumber + 30)) {
-					$('#feedback').text('Cold!');
-				}
-				else if (guess < (mysteryNumber - 20) && guess > (mysteryNumber + 20)) {
-					$('#feedback').text('Warm!');
-				}
-				else if (guess < (mysteryNumber - 10) && guess > (mysteryNumber + 10)) {
-					$('#feedback').text('Hot!');
-				}
-				else if (guess < (mysteryNumber - 5) && guess > (mysteryNumber + 5)) {
-					$('#feedback').text('Very hot!');
-				}
-				else {
-					$('#feedback').text('Extremely hot!');
-				}
-		
-			$('#userGuess').val('');
-		})
-		})
-	}
-
-});
-
-
-
-/*$('.new').click(function() {
-			$('#feedback, #userGuess, #guessButton, #count').reset();
-		})*/
-
-
-	
-
-
 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -94,6 +44,24 @@ function newGame() {
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+
+});
+
+	/*(function newGame() {
+		$('.new').click(function() {
+
+	})();*/
+
+/*$('.new').click(function() {
+			$('#feedback, #userGuess, #guessButton, #count').reset();
+		})*/
+
+	
+
+
+
+	
 
   
 
